38b44151f33be914b28c50423ca573362e369c05
夏千祥 <satanrabbit@gmail.com>
2013-04-30 19:20:42 +0800

    探究身份验证

--- Muse/Controllers/HomeController.cs
+++ Muse/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace Muse.Controllers
 {
@@ -10,12 +11,44 @@ namespace Muse.Controllers
     {
         //
         // GET: /Home/
-
+        [ValidateInput(false)]
         public ActionResult Index()
         {
+            //FormsAuthenticationTicket
+            if (User.Identity.IsAuthenticated)
+            {
+                int userID = Convert.ToInt32(User.Identity.Name);
+                var monitor = 3;
+                if (monitor != null)
+                {
+                    if (userID == 3)
+                    {
+                        //设置Cookies，指定权限,返回至登陆前页面
+                        FormsAuthenticationTicket authTicket = new FormsAuthenticationTicket(
+                            1,
+                            userID.ToString(),
+                            DateTime.Now,
+                            DateTime.Now.AddHours(10000),
+                            false,
+                           "monitor,student"
+                            );
+                        string encTicket = FormsAuthentication.Encrypt(authTicket);
+                        this.Response.Cookies.Add(new HttpCookie(FormsAuthentication.FormsCookieName, encTicket));
+                        string url = Request["ReturnUrl"];
+                        if (url == null)
+                        {
+                            return RedirectToAction("Index", "MClass");
+                        }
+                        else
+                        {
+                            return Redirect(url);
+                        }
+                    }
+                }
+            }
             return View();
         }
-
+        [Authorize]
         public ActionResult Guide()
         {
             return View();
--- Muse/Global.asax.cs
+++ Muse/Global.asax.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Security;
 
 namespace Muse
 {
@@ -22,6 +24,24 @@ namespace Muse
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+        protected void Application_AuthenticateRequest()
+        {
+            //认证授权
+            if (HttpContext.Current.User != null)
+            {
+                if (HttpContext.Current.User.Identity.IsAuthenticated)
+                {
+                    if (HttpContext.Current.User.Identity is FormsIdentity)
+                    {
+                        FormsIdentity id = (FormsIdentity)HttpContext.Current.User.Identity;
+                        FormsAuthenticationTicket ticket = id.Ticket;
+                        string userData = ticket.UserData;
+                        string[] roles = userData.Split(',');
+                        HttpContext.Current.User = new GenericPrincipal(id, roles);
+                    }
+                }
+            }
         }
     }
 }
